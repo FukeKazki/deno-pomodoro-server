b072b0c990a64d2caeca1b2b2604cd800a6c9dfa
FukeKazki <kazkichi0906@gmail.com>
2022-08-06 19:32:16 +0900

    cors

--- serve.ts
+++ serve.ts
@@ -4,6 +4,10 @@ import router from './router.ts'
 
 const app = new Application();
 
+app.use((ctx, next) => {
+  ctx.response.headers.set('Access-Control-Allow-Origin', '*')
+  return next()
+})
 app.use(router.routes())
 app.use(router.allowedMethods())
 
